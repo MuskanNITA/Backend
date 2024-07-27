b2ef1a2b76e21f5fbee352def5f819ecf951ebc6
Muskan Gupta <115357215+MuskanNITA@users.noreply.github.com>
2024-07-27 10:00:23 +0530

    Update book.controller.js

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -9,42 +9,5 @@ export const getBook = async(req, res) => {
         res.status(500).json(error);
     }
 };
-/*
-import Book from '../model/book.model.js';
 
-// Function to get all books
-export const getBook = async (req, res) => {
-  try {
-    const books = await Book.find();
-    res.status(200).json(books);
-  } catch (error) {
-    console.log("Error: ", error);
-    res.status(500).json(error);
-  }
-};
-
-// Function to create a new book
-export const postBook = async (req, res) => {
-  // Extract book details from request body
-  const { name, price, category, image, title } = req.body;
-
-  try {
-    // Create a new book instance
-    const newBook = new Book({
-      name,
-      price,
-      category,
-      image,
-      title
-    });
-
-    // Save the new book to the database
-    await newBook.save();
-
-    // Send success response
-    res.status(201).json(newBook);
-  } catch (error) {
-    console.log("Error: ", error);
-    res.status(400).json({ message: 'Error creating book', error });
-  }
-};*/
+    
